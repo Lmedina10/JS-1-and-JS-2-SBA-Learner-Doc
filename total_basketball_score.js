4b3772a92189a3fdaea9261011ff021d8665b474
LM <Luismedina173@gmail.com>
2022-07-28 12:03:18 -0400

    Done with the totalBasketballScore

---
+++ total_basketball_score.js
@@ -0,0 +1,22 @@
+// function totalBasketballScore(ft, mr, tp) => {
+//     if(typeof ft === 'number' && typeof mr === 'number' && typeof tp === 'number')
+//         return (ft * 1) + (mr * 2) + (tp * 3);
+//      {
+
+//     }else{
+//         return 'updating score'
+//     }
+// }
+
+// console.Log(totalBasketballScore(2, 3, 4))
+
+
+const total = 'Total baseball score'
+function totalBasketballScore(ft, mr, tp) {
+    if (typeof ft === 'number' && typeof mr === 'number' && typeof tp === 'number') {
+      return (ft * 1) + (mr * 2) + (tp * 3);
+    }
+    return 'pending score' ;
+  }
+  
+  console.log(totalBasketballScore(2, 3, 4));
